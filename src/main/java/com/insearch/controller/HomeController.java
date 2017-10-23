b9f22417e48a9a0f43d5aa94a44f57221bf82c5b
xorwnsnmnnhlba <xorwnsnmnn@naver.com>
2017-10-23 19:47:32 +0900

    #001 inSearch 프로젝트 생성, 깃허브에 올려두기
    - 기본 Setting 작업 진행.

--- src/main/java/com/insearch/controller/HomeController.java
+++ src/main/java/com/insearch/controller/HomeController.java
@@ -1,39 +1,31 @@
 package com.insearch.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import io.swagger.annotations.Api;
 
 /**
  * Handles requests for the application home page.
  */
+
 @Controller
+@Api(value="swagger", description="swagger-test")
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	public String home() {
 		return "home";
 	}
 	
+	@RequestMapping(value="/swagger", method = RequestMethod.GET)
+	public String homePost(){
+		return "redirect:/swagger-ui.html";
+	}
+
 }
---
+++ src/main/java/com/insearch/controller/MapController.java
@@ -0,0 +1,9 @@
+package com.insearch.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MapController {
+	
+
+}
